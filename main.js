2924a39f7c85c2decb70a6d435ed3274079faea5
DataRedact <mriskool00@gmail.com>
2021-04-14 23:49:24 +0100

    Changed role names to fit

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ client.on("message", msg => {
 client.on("message", msg => {
     const { guild } = msg;
     const verifiedRole = guild.roles.cache.find((role => role.name == 'Hypixel Verified'));
-    const sentenciaRole = guild.roles.cache.find((role => role.name == 'Sentencia Member'));
+    const sentenciaRole = guild.roles.cache.find((role => role.name == 'Guild: Sentencia Eternal'));
     const member = guild.members.cache.get(msg.author.id);
     if (msg.content.startsWith(prefix + 'unverify')) {
         member.setNickname(msg.author.username)
